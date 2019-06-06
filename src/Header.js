b236ec8b60fa143bdf4a9a03adbd5fdcc0b5f128
Adel <htwr86@gmail.com>
2019-06-06 22:59:46 +0300

    add centered container

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,13 @@ const Header = () => {
       <div className="tc m3 pv4 pv5-ns bg-black-30">
         <h1 className='f1'>It's Header</h1>
       </div>
-      
+      <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+        <h1 class="mt0">Centered Container</h1>
+        <p class="lh-copy measure">
+          Combine width or max-width values with the center class to create a centered
+          container for your content.
+        </p>
+      </section>
     </div>
   )
 }
