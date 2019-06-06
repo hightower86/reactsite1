e45827a7f0152093b9a81a0bbadc002e5e5cc5f4
Adel <htwr86@gmail.com>
2019-06-06 18:14:30 +0300

    add header class

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { directive } from '@babel/types';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="black-90">
+        <h1 className='f'>It's Header</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Header;
