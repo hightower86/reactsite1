18f5eafc93c716199dcbfc3f4e3e040754165c55
Adel <htwr86@gmail.com>
2019-06-07 23:16:31 +0300

    some changes bi section1

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header class="flex items-center bg-black-70 h4 ph3 pv3 mb2 pv3-ns ph-m ph4-l">
-      <nav class="fl w-100 w-70-ns pa2 f6 fw6 ttu tracked">
+    <header class="flex items-center bg-black-70 h4 ph3 mb2">
+      <nav class="fl w-100 w-60-m pa2 vh-75-mf6 fw6 ttu tracked">
         <a class="link dim white dib mr3 ba br2 pa2" href="" title="Home">Домой</a>
         <a class="link dim white dib mr3  ba br2 pa2" href="" title="About">О компании</a>
         <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Store">Магазин</a>
         <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Contact">Контакты</a>
       </nav>
-      <div className='fl w-100 w-30-ns pa2 white f3'>
+      <div className='flex justify-end fl w-100 w-40-m pa2 white f3-l f4-ns'>
         phone: +7 (908) 893-65-50
       </div>
 
