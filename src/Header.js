946c637dec6f2e4cd16930709aa4ffe1a3d4e5f6
Adel <htwr86@gmail.com>
2019-06-07 22:14:56 +0300

    header add grid

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,16 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header class="flex justify-around bg-black-70  w-100 ph3 pv3 mb2 pv3-ns ph-m ph4-l">
-      <nav class="f6 fw6 ttu tracked">
+    <header class="flex items-center bg-black-70 h4 ph3 pv3 mb2 pv3-ns ph-m ph4-l">
+      <nav class="fl w-100 w-70-ns pa2 f6 fw6 ttu tracked">
         <a class="link dim white dib mr3 ba br2 pa2" href="" title="Home">Домой</a>
         <a class="link dim white dib mr3  ba br2 pa2" href="" title="About">О компании</a>
         <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Store">Магазин</a>
         <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Contact">Контакты</a>
       </nav>
-      <span className='white f3'>phone: +7 (908) 893-65-50</span>
+      <div className='fl w-100 w-30-ns pa2 white f3'>
+        phone: +7 (908) 893-65-50
+      </div>
 
     </header>
   )
--- src/Section1.js
+++ src/Section1.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 const Section1 = () => {
   return (
-    <div pa3>
-      <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+    <div>
+      <section class="fl w-100 w-50-ns mw5 mw7-ns bg-light-gray pa2 ph5-ns">
         <h1 class="mt0">Centered Container</h1>
         <p class="lh-copy measure">
           Combine width or max-width values with the center class to create a centered
           container for your content.
         </p>
       </section>
+      <section class="fl w-100 w-50-ns mw5 mw7-ns bg-light-gray pa2 ph5-ns">
+        <h1 class="mt0">Centered Container</h1>
+        <p class="lh-copy measure">
+          Combine width or max-width values with the center class to create a centered
+          container for your content.
+        </p>
+      </section>
+
+
     </div>
 
   );
