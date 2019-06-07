0e1f9b1da0d16b1c1275047c9f5a24c163c72bf9
Adel <htwr86@gmail.com>
2019-06-07 17:27:46 +0300

    add batton styles in header

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,15 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='h-100'>
-      <div className="tc m3 pv4 pv5-ns bg-black-30">
-        <h1 className='f1'>It's Header</h1>
-      </div>
+    <div>
+      <header class="bg-black-70 fixed w-100 ph3 pv3 pv4-ns ph4-m ph4-l">
+        <nav class="f6 fw6 ttu tracked">
+          <a class="link dim white dib mr3 ba br2 pa2" href="" title="Home">Домой</a>
+          <a class="link dim white dib mr3  ba br2 pa2" href="" title="About">О компании</a>
+          <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Store">Магазин</a>
+          <a class="link dim white dib ba br2 pa2" href="#" title="Contact">Контакты</a>
+        </nav>
+      </header>
       <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
         <h1 class="mt0">Centered Container</h1>
         <p class="lh-copy measure">
