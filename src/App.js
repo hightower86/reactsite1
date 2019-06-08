25030cd398bd7fe6d97ce52190f33c1b25d84a67
Adel <htwr86@gmail.com>
2019-06-08 18:19:58 +0300

    add maket Its

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 //import './App.css';
 import Header from './Header' ;
 import Section1 from './Section1';
+import Its from './Its';
 import Footer from './Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="">
       <Header />
       <Section1 />
+      <Its />
       <Footer />
     </div>
   );
---
+++ src/Its.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Its = () => {
+  return (
+    <div className="vh-75 bg-yellow tc pt4">
+      <h2 >Услуги сопровождения 1С ИТС</h2>
+    </div>
+  )
+}
+
+export default Its;
