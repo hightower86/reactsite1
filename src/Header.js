2abe620ac220e57b5d230124770d662466e7de89
Adel <htwr86@gmail.com>
2019-06-08 17:11:54 +0300

    nav updated

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header class="flex items-center bg-black-70 h4 ph3 mb2">
-      <nav class="fl w-100 w-60-m pa2 vh-75-mf6 fw6 ttu tracked">
-        <a class="link dim white dib mr3 ba br2 pa2" href="" title="Home">Домой</a>
-        <a class="link dim white dib mr3  ba br2 pa2" href="" title="About">О компании</a>
-        <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Store">Магазин</a>
-        <a class="link dim white dib mr3 ba br2 pa2" href="#" title="Contact">Контакты</a>
+    <header class="w-100 sticky flex items-center bg-black-70 h3 ph3">
+      <nav class="fl w-100-m w-60-l pa2 vh-75-mf6 fw6 ttu tracked">
+        <a class="link white-60 mr3 pa2" href="" title="Home">Домой</a>
+        <a class="link white-60 mr3 pa2" href="" title="About">О компании</a>
+        <a class="link white-60 mr3 pa2" href="" title="Store">Магазин</a>
+        <a class="link white-60 mr3 pa2" href="" title="Contact">Контакты</a>
       </nav>
-      <div className='flex justify-end fl w-100 w-40-m pa2 white f3-l f4-ns'>
+      <div className='flex justify-end fl w-100-m w-40-l pa2 white f3-l f4-ns'>
         phone: +7 (908) 893-65-50
       </div>
 
--- src/Section1.js
+++ src/Section1.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 const Section1 = () => {
   return (
-    <div>
-      <section class="fl w-100 w-50-ns mw5 mw7-ns bg-light-gray pa2 ph5-ns">
-        <h1 class="mt0">Centered Container</h1>
-        <p class="lh-copy measure">
+    <div className='mw9 bg-light-gray'>
+      <h2 className='pa3 tc f2'>Наши услуги.</h2>
+      <section className="flex items-center vh-50 fl w-100-m w-50-ns mw5 mw9-ns pa2 ph5-ns">
+        <div className=''>
+          <h1 className="mt0">Centered Container</h1>
+          <p className="lh-copy measure">
           Combine width or max-width values with the center class to create a centered
           container for your content.
-        </p>
+          </p>
+        </div>
       </section>
-      <section class="fl w-100 w-50-ns mw5 mw7-ns bg-light-gray pa2 ph5-ns">
-        <h1 class="mt0">Centered Container</h1>
-        <p class="lh-copy measure">
+      <section className="flex items-center vh-50 fl w-100-m w-50-ns mw5 mw9-ns pa2 ph5-ns">
+        <div className=''>
+          <h1 className="mt0">Centered Container</h1>
+          <p className="lh-copy measure">
           Combine width or max-width values with the center class to create a centered
           container for your content.
-        </p>
+          </p>
+        </div>
       </section>
 
 
