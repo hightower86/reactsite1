79672c6bbc55c0f31e33268fa1fd2e921cd4e6fb
Adel <htwr86@gmail.com>
2019-06-09 20:28:42 +0300

    add css grid

--- src/Main.js
+++ src/Main.js
@@ -1,86 +1,93 @@
 import React from 'react';
+import Footer from './Footer';
+import './main.css';
 
 const Main = () => {
   return (
     <div className='mw10 bg-light-gray'>
-      <h2 className='pa3 tc f2'>Наши услуги.</h2>
-      <section className="tc bg-white-50 dib br2 ba b--white pa3 ma2 bw1 shadow-hover:hover::after">
-        <div className=''>
-          <h2 className="f3 mt0 pb3 mh3">1С Бухгалтерия 8</h2>
-          <p className="lh-copy measure pb3 mb3">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          <br></br>
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
+      <h2 className='pa3 tc f2'>Программы 1С:Предприятие 8</h2>
+      <h4 className='f4 ph6 pb3 tc'>Сегодня доступна не только покупка лицензии на программу и установка на свой компьютер, но и пользоваться программой по подписке (аренда в облачном сервисе).</h4>
+      <div className='main'>
+        <section className="tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
+          <div className=''>
+            <h2 className="f3 mt0 pb3 mh3">1С Бухгалтерия 8</h2>
+            <p className="lh-copy measure pb3 mb3">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+            <p className='lh-copy measure pb3 mb3'>
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
 
-        </div>
-      </section>
-      <section className="tc bg-light-blue dib br0 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <hr className='pb2'/>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
-      <section className="tc bg-light-green dib br1 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
-      <section className="tc bg-light-gray dib br1 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
-      <section className="tc bg-light-gray dib br2 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
-      <section className="tc bg-light-blue dib br2 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
-      <section className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
-      <section className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-3">
-        <div className=''>
-          <h1 className="mt0">Centered Container</h1>
-          <p className="lh-copy measure">
-          Combine width or max-width values with the center class to create a centered
-          container for your content.
-          </p>
-        </div>
-      </section>
+          </div>
+        </section>
+        <section className="tc bg-light-blue dib br0 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <hr className='pb2'/>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+        <section className="tc bg-light-green dib br1 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+        <section className="tc bg-light-gray dib br1 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+        <section className="tc bg-light-gray dib br2 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+        <section className="tc bg-light-blue dib br2 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+        <section className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+        <section className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-3">
+          <div className=''>
+            <h1 className="mt0">Centered Container</h1>
+            <p className="lh-copy measure">
+            Combine width or max-width values with the center class to create a centered
+            container for your content.
+            </p>
+          </div>
+        </section>
+      </div>
+      
     </div>
   );
 }
