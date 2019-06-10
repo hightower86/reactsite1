a8664e9310b44acc1b6d0be1e86af2ac9cf2113d
Adel <htwr86@gmail.com>
2019-06-10 21:03:36 +0300

    product cards

--- src/Main.js
+++ src/Main.js
@@ -7,14 +7,52 @@ const Main = () => {
   return (
     <div className='mw10 bg-light-gray'>
       <h2 className='animated flipInX pa3 tc f2'>Программы 1С:Предприятие 8</h2>
-      <h4 className='ad1 animated flipInX f4 ph6 pb3 tc'>Сегодня доступна не только покупка лицензии на программу и установка на свой компьютер, но и пользоваться программой по подписке (аренда в облачном сервисе).</h4>
+      <h4 className='ad1 animated flipInX f4 ph6 pb3 tc'>Купить или арендовать? Правильный выбор можно сделать после ответа на вопросы.</h4>
       <div className='main'>
         <section className="ad2 animated bounceInLeft  bg-animate hover-bg-light-yellow tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
           <div className=''>
-            <h2 className="f3 mt0 pb3 mh3">1С Бухгалтерия 8</h2>
+            <h2 className="f3 mt0 pb3 mh3">Покупка</h2>
             <p className="lh-copy measure pb3 mb3">
-            Combine width or max-width values with the center class to create a centered
-            container for your content.
+            <div className='b tl ml3'>Преимущества: </div>
+            <ul className='ml4 tl'>
+              <li>база всегда "при себе". доступ только для своих.</li>
+              <li>заплатил один раз и пользуйся</li>
+              <li>низкая стоимость владения для базовых версий</li>
+            </ul>
+            <div className='b tl ml3'>Недостатки: </div>
+            <ul className='ml4 tl'>
+              <li>если не соблюдать правила, легко потерять данные</li>
+              <li>любое масштабирование связано с доп. затратами</li>
+              <li>сопровождение конфигураций дороже.</li>
+            </ul>
+            </p>
+          </div>
+        </section>
+        <section className="ad3 animated bounceInRight  bg-animate hover-bg-light-yellow tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
+          <div className=''>
+            <h2 className="f3 mt0 pb3 mh3">Аренда</h2>
+            <p className="lh-copy measure pb3 mb3">
+            <div className='b tl ml5'>Преимущества: </div>
+            <ul className='ml4 tl'>
+              <li>база в надежном месте.</li>
+              <li>доступ из любой точки планеты.</li>
+              <li>легкое масштабирование</li>
+            </ul>
+            <div className='b tl ml5'>Недостатки: </div>
+            <ul className='ml4 tl'>
+              <li>нет интернета - нет доступа к программе</li>
+              <li>заплатил один раз и пользуйся</li>
+              <li></li>
+            </ul>
+            </p>
+          </div>
+        </section>
+        <section className="ad2 animated bounceInLeft  bg-animate hover-bg-light-yellow tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
+          <div className=''>
+            <h2 className="f3 mt0 pb3 mh3">1С Бухгалтерия</h2>
+            <p className="lh-copy measure pb3 mb3">
+            <div className='b tl m5'>Преимущества: </div>
+       
             </p>
             <p className='lh-copy measure pb3 mb3'>
             Combine width or max-width values with the center class to create a centered
@@ -27,68 +65,78 @@ const Main = () => {
             
           </div>
         </section>
-        <section className="ad3 animated bounceInRight tc bg-light-blue dib br0 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
+        <section className="ad2 animated bounceInRight  bg-animate hover-bg-light-yellow tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
           <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <hr className='pb2'/>
-            <p className="lh-copy measure">
+            <h2 className="f3 mt0 pb3 mh3">1С Зарплата и Управление Персоналом</h2>
+            <p className="lh-copy measure pb3 mb3">
+            <div className='b tl m5'>Преимущества: </div>
+            
+            </p>
+            <p className='lh-copy measure pb3 mb3'>
             Combine width or max-width values with the center class to create a centered
             container for your content.
             </p>
+            <div className="btns pb3">
+              <a className="h-btn ba br1 pa3">Купить</a>
+              <a className="ba br1 pa3 grow">Арендовать</a>
+            </div>
+            
           </div>
         </section>
-        <section className="tc bg-light-green dib br1 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
+        <section className="ad2 animated bounceInLeft  bg-animate hover-bg-light-yellow tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
           <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <p className="lh-copy measure">
+            <h2 className="f3 mt0 pb3 mh3">Покупка</h2>
+            <p className="lh-copy measure pb3 mb3">
+            <div className='b tl m5'>Преимущества: </div>
+            <ul className='ml4 tl'>
+              <li>база всегда "при себе". доступ только для своих.</li>
+              <li>заплатил один раз и пользуйся</li>
+              <li></li>
+            </ul>
+            <div className='b tl m5'>Недостатки: </div>
+            <ul className='ml4 tl'>
+              <li>база всегда "при себе". доступ только для своих.</li>
+              <li>заплатил один раз и пользуйся</li>
+              <li></li>
+            </ul>
+            </p>
+            <p className='lh-copy measure pb3 mb3'>
             Combine width or max-width values with the center class to create a centered
             container for your content.
             </p>
+            <div className="btns pb3">
+              <a className="h-btn ba br1 pa3">Купить</a>
+              <a className="ba br1 pa3 grow">Арендовать</a>
+            </div>
+            
           </div>
         </section>
-        <section className="tc bg-light-gray dib br1 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
+        <section className="ad2 animated bounceInRight  bg-animate hover-bg-light-yellow tc bg-white-50 dib br2 pa3 ma2 bw1 shadow-3">
           <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <p className="lh-copy measure">
+            <h2 className="f3 mt0 pb3 mh3">Покупка</h2>
+            <p className="lh-copy measure pb3 mb3">
+            <div className='b tl m5'>Преимущества: </div>
+            <ul className='ml4 tl'>
+              <li>база всегда "при себе". доступ только для своих.</li>
+              <li>заплатил один раз и пользуйся</li>
+              <li></li>
+            </ul>
+            <div className='b tl m5'>Недостатки: </div>
+            <ul className='ml4 tl'>
+              <li>база всегда "при себе". доступ только для своих.</li>
+              <li>заплатил один раз и пользуйся</li>
+              <li></li>
+            </ul>
+            </p>
+            <p className='lh-copy measure pb3 mb3'>
             Combine width or max-width values with the center class to create a centered
             container for your content.
             </p>
-          </div>
-        </section>
-        <section className="tc bg-light-gray dib br2 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
-          <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <p className="lh-copy measure">
-            Combine width or max-width values with the center class to create a centered
-            container for your content.
-            </p>
-          </div>
-        </section>
-        <section className="tc bg-light-blue dib br2 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
-          <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <p className="lh-copy measure">
-            Combine width or max-width values with the center class to create a centered
-            container for your content.
-            </p>
-          </div>
-        </section>
-        <section className="tc bg-light-red dib br3 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
-          <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <p className="lh-copy measure">
-            Combine width or max-width values with the center class to create a centered
-            container for your content.
-            </p>
-          </div>
-        </section>
-        <section className="tc bg-light-green dib br3 pa3 ma2 bg-animate hover-bg-light-yellow bw2 shadow-3">
-          <div className=''>
-            <h1 className="mt0">Centered Container</h1>
-            <p className="lh-copy measure">
-            Combine width or max-width values with the center class to create a centered
-            container for your content.
-            </p>
+            <div className="btns pb3">
+              <a className="h-btn ba br1 pa3">Купить</a>
+              <a className="ba br1 pa3 grow">Арендовать</a>
+            </div>
+            
           </div>
         </section>
       </div>
